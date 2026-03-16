42356875087da831046cc89ec6085a2b3abbdf66
RafaelGandolfiG <rafaelgandolfi0@gmail.com>
2026-03-16 13:56:23 -0300

    aula02

---
+++ aula02/aula02-desafios.py
@@ -0,0 +1,15 @@
+# DESAFIO 1
+nome=input('Digite seu nome: ')
+print(f'Bem-vindo {nome}')
+
+# DESAFIO 2
+dia=input('Digite o dia do seu nascimento: ')
+mes=input('Digite o mes de seu nascimento: ')
+ano=input('Digite o ano de seu nascimento: ')
+print(f'{dia}/{mes}/{ano}')
+
+# DESAFIO 3
+n1=int(input('Digite o numero 1: '))
+n2=int(input('Digite o numero 2: '))
+soma=n1+n2
+print(f'A soma é {soma}')
---
+++ aula02/aula02.py
@@ -0,0 +1,30 @@
+print('hello, world')
+
+print(7+4)
+print('7 + 4')
+print('7' + '4') # Estou concatenando strings
+
+# São comentários de uma linha no python
+'''
+    Isso são 
+    comentários 
+    de múltiplas 
+    linhas
+'''
+
+# VARIÁVEIS
+# a variável nome recebe o texto "rafael" e guarda na memória
+nome='Rafael'
+idade=19
+peso=72.4
+
+print(nome, idade, peso)
+print(f'Oiii, {nome}')
+
+# INPUT - FORMULÁRIO SIMPLS NO PROMPT DE COMANDO (CMD)
+
+nome=input('Digite seu nome: ')
+idade=input('Digite sua idade: ')
+peso=input('Digite o seu peso: ')
+
+print(nome, idade, peso)
