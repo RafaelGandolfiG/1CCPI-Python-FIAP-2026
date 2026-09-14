64bca13921999482b5cf43be1a45990462462ba4
Laboratório FIAP <labsfiap@fiap.com.br>
2026-09-14 11:39:51 -0300

    aula08

---
+++ aula08/app.py
@@ -0,0 +1,34 @@
+from model import model_lead
+import control
+
+def add_lead():
+    name=input('nome: ')
+    email=input('e-mail: ')
+    status=input('etapa no funil de vendas: ')
+    print('lead adicionado')
+    print(model_lead(name,email,status))
+    control.create_lead(model_lead(name,email,status))
+
+def list_leads():
+    leads=control.read_leads()
+    
+
+def main():
+    while True:
+        print('\nMini CRM de Leads')
+        print('[1] Adicionar Lead')
+        print('[2] Listar Leads')
+        print('[0] Sair do programa')
+        opt=input('Escolha uma opção: ')
+        if opt=='1':
+            add_lead()
+        elif opt=='2':
+            list_leads()
+        elif opt=='0':
+            print('Saindo...')
+            break
+        else:
+            print('opção invalida')
+
+if __name__=='__main__':
+    main()
